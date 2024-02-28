3edba1fb9a0748680233a95c7bbf7d9642bc9bba
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-02-28 16:45:19 +0300

    Option Pattern (appsetting.json)
    Using the Option Pattern, configuration operations were combined into a single class and the program.this is defined in cs. During the follow-up, the name and path of the database were adjusted. // Option Pattern kullanılarak configuration işlemleri tek bir sınıfta birleştirildi ve program.cs’de bu tanımlandı. Takibinde veri tabanı’nın ismi ve yolu ayarlandı.

--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,3 +1,5 @@
+using FreeCourse.Services.Catalog.Settings;
+using Microsoft.Extensions.Options;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSetting"));
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value; // "GetRequiredService" will throw an error if it cannot find the corresponding service. // "GetRequiredService" eðer ilgili servisi bulamazsa hata fýrlatacak.
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
