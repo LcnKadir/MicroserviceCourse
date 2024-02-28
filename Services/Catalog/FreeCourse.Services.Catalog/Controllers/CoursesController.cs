f6bc456589b1021e76ff3542a9483e780527985b
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-02-28 19:27:56 +0300

    CoursesController
    CoursesController oluşturularak, methodların endpoint işlemleri yazıldı. Controller’lar da durum kodlarını kullanabilmek adına, Shared’da BaseController oluşturuldu.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
@@ -0,0 +1,67 @@
+﻿using FreeCourse.Services.Catalog.DTOs;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.BaseController;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal  class CoursesController : CustomBaseController
+    {
+        private readonly ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _courseService.GetAllAsync();
+
+            return CreateActionResultInstance(response);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _courseService.GetByIdAsync(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [Route("/api/[controller]/GetAllByUserId/{userId}")] //Separated the Route and Id methods from each other. //Route ile Id methodlarını birbirinden ayrıldı.
+        public async Task<IActionResult> GetAllByUserId(string userId)
+        {
+            var response = await _courseService.GetAllByUserIdAsync(userId);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseCreateDto courseCreateDto)
+        {
+            var response = await _courseService.CreateAsync(courseCreateDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(CourseUpdateDto  courseUpdateDto)
+        {
+            var response = await _courseService.UpdateAsync(courseUpdateDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var response = await _courseService.DeleteAsync(id);
+            return CreateActionResultInstance(response);
+        }
+    }
+}
---
+++ Shared/FreeCourse.Shared/BaseController/CustomBaseController.cs
@@ -0,0 +1,21 @@
+﻿using FreeCourse.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Shared.BaseController
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode //It will receive the status codes from Response directly. //Response'dan gelen durum kodlarını direkt alacak.
+            };
+        }
+    }
+}
