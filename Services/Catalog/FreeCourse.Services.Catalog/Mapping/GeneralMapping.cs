62aec27f26a2d70229fbae9252aa5977acf120d2
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-02-28 16:07:35 +0300

    AutoMapper
    The AutoMapper library has been added to the project. The mapper was defined in the DI Container, and the GeneralMapping class was created for the DTO conversion process. // Projeye AutoMapper kütüphanesi eklendi. Mapper, DI Container’da tanımlandı, takibinde DTO dönüştürme işlemi için GeneralMapping class’ı oluşturuldu.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.DTOs;
+using FreeCourse.Services.Catalog.Models;
+
+namespace FreeCourse.Services.Catalog.Mapping
+{
+    public class GeneralMapping: Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Category,CategortDto>().ReverseMap();
+            CreateMap<Course,CourseDto>().ReverseMap();
+            CreateMap<Feature,FeatureDto>().ReverseMap();
+
+            CreateMap<Course,CourseCreateDto>().ReverseMap();
+            CreateMap<Course, CoruseUpdateDto>().ReverseMap();
+        }
+    }
+}
--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly()); // Added AutoMapper to convert DTOs. // DTO'larý dönüþtürmek için AutoMapper'ý eklendi.
+
 
 var app = builder.Build();
 
