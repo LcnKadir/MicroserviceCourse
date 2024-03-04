64da2f7c13daf913a2717a893dc624d20513edec
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-04 15:25:16 +0300

    FreeCourse.Service.Catolog (JWT)
    The JWT package has been installed to protect the Microservice and the program.the necessary encodings have been written in cs.During the follow-up, endpoint tests were performed with Postman.// Microservice'i koruma altına almak için JWT paketi yüklendi ve program.cs ‘de gerekli kodlamalar yazıldı. Takibinde Postman ile endpoint testleri gerçekleşti.

--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,5 +1,7 @@
 using FreeCourse.Services.Catalog.Services;
 using FreeCourse.Services.Catalog.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
 using System.Reflection;
 
@@ -7,7 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt => //Authorize has been added to protect all controllers. This way we won't have to pass Authorize on every controller.
+                                       //Tüm controller'larý koruma altýna almak için Authorize eklemesi yapýldý. Böylelikle her controller'da  Authorize etribütünü geçmek zorunda kalmayacaðýz.
+{
+    opt.Filters.Add(new AuthorizeFilter());
+}); 
 
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICourseService, CourseService>();
@@ -18,6 +24,16 @@ builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("D
 builder.Services.AddSingleton<IDatabaseSettings>(sp =>
 {
     return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value; // "GetRequiredService" will throw an error if it cannot find the corresponding service. // "GetRequiredService" eðer ilgili servisi bulamazsa hata fýrlatacak.
+});
+
+
+//JSONWEBTOKEN
+//Microservice is protected. // Microservice'i koruma altýna aldýk.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_catalog";
+    options.RequireHttpsMetadata = false;
 });
 
 
@@ -35,7 +51,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
