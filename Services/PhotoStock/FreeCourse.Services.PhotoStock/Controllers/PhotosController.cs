e9e613acaebb6d91c65e43a5641b6bf566956f4d
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-05 13:12:24 +0300

    PhotosController (PhotoDelete)
     The photo was written to the PhotoDelete for deletion operations. In order to protect the Microservice, the JWT package has been installed and the program.added to cs. // Fotoğraf silme işlemleri için PhotoDelete yazıldı. Microservice'i koruma altına almak adına JWT paketi yüklendi ve program.cs ‘e eklendi.

--- Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -21,7 +21,7 @@ namespace FreeCourse.Services.PhotoStock.Controllers
                 var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
 
                 using (var stream = new FileStream(path, FileMode.Create))
-                await photo.CopyToAsync(stream, cancellationToken);
+                    await photo.CopyToAsync(stream, cancellationToken);
 
                 var returnPath = "photos/" + photo.FileName;
 
@@ -31,7 +31,20 @@ namespace FreeCourse.Services.PhotoStock.Controllers
 
             }
 
-            return CreateActionResultInstance (Response<PhotoDto>.Fail("photo is empty", 400));
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("photo is empty", 400));
+        }
+
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("photo not found", 404));
+            }
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
         }
     }
 }
--- Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
@@ -1,8 +1,27 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt => 
+                                      
+{
+    opt.Filters.Add(new AuthorizeFilter());
+});
+
+
+//JSONWEBTOKEN
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_catalog";
+    options.RequireHttpsMetadata = false;
+});
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,6 +35,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseStaticFiles();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
