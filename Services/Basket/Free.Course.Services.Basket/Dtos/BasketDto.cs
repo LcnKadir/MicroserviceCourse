53438066058a4c3489b01d99a599748da6904b52
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-06 12:05:25 +0300

    Created BasketItemDto and BasketDto

---
+++ Services/Basket/Free.Course.Services.Basket/Dtos/BasketDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Free.Course.Services.Basket.Dtos
+{
+    public class BasketDto
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public List<BasketItemDto> BasketItems { get; set; }
+
+        public decimal TotalPrice
+        {
+            get => BasketItems.Sum(x => x.Price* x.Quantity);
+        }
+    }
+}
