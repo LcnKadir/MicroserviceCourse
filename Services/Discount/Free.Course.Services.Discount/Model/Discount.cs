0d274f8026632af91da8469bf0c8044d63f5ef05
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-07 15:26:43 +0300

    Dapper Library and Models
    The Dapper library was included in the project. The model folder was created and the entities were encoded. // Dapper kütüphanesi projeye dahil edildi. Model klasörü oluşturuldu ve entity'ler kodlandı.

---
+++ Services/Discount/Free.Course.Services.Discount/Model/Discount.cs
@@ -0,0 +1,13 @@
+﻿namespace Free.Course.Services.Discount.Model
+{
+    [Dapper.Contrib.Extensions.Table("discount")]
+    public class Discount
+    {
+
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int Rate { get; set; }
+        public string Code { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
