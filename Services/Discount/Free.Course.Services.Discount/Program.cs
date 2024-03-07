2ac3c442d238bc84b0ffd68b75e5605cab27857c
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-07 17:16:01 +0300

    Microservice protection
    To protect the microservice to the DI Container and appsettings.the corresponding Identity links have been added to the json. // Microservice’i korumak için DI Container’a  ve appsettings.json’a ilgili İdentity bağlantıları eklendi.

--- Services/Discount/Free.Course.Services.Discount/Program.cs
+++ Services/Discount/Free.Course.Services.Discount/Program.cs
@@ -1,8 +1,37 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
+
+
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+//JSONWEBTOKEN
+//Microservice is protected. 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_discount";
+    options.RequireHttpsMetadata = false;
+});
+
+
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,7 +44,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
