0df8aff4a33830e3f7f84091437fc7fcc1dce48a
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-10 22:29:33 +0300

    ApplicationLibrary (AutoMapper)
    Free.Course.Services.Order.Application’da Entity’leri Dto’ya dönüştürmek için AutoMapper kütüphanesi eklendi. AutoMapper kütüphanesi kullanmak için StaticClass oluşturuldu.

---
+++ Services/Order/Free.Course.Services.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Free.Course.Services.Order.Application.Dtos;
+using Free.Course.Services.Order.Domain.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Free.Course.Services.Order.Application.Mapping
+{
+     class CustomMapping: Profile
+    {
+        public CustomMapping() {
+            CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Address, AddressDto>().ReverseMap();
+        
+        }
+    }
+}
---
+++ Services/Order/Free.Course.Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Free.Course.Services.Order.Application.Mapping
+{
+    public static class ObjectMapper
+    {
+        //Lazy will initialize only when using automapper, not when the project stands up. //Lazy proje ayağa kalktığında değil, sadece automapper kullanımında initialization edecek.
+
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CustomMapping>();
+            });
+
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper => lazy.Value;
+    }
+}
