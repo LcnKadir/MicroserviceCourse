638135929067ce82eb913791f1d41ff78a453014
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-10 18:25:41 +0300

    DomainLibrary(OrderAggregate)
    The definition of the “OrderItem” class, which will inherit the base classes and has its equivalent in the database, has been performed. // Base class’ları miras alacak olan ve veri tabanında karşılığı bulunan “OrderItem” class’ın tanımlaması gerçekleştirildi.

---
+++ Services/Order/Free.Courser.Services.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Free.Course.Services.Order.Domain.OrderAggregate
+{
+    public class OrderItem
+    {
+        public string ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public string PictureUrl { get; private set; }
+        public Decimal Price { get; private set; }
+
+        public OrderItem(string productId, string productName, string pictureUrl, decimal price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+            Price = price;
+        }
+
+        public void UpdateOrderItem(string productName, string pictureUrl, decimal price)
+        {
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+            Price = price;
+        }
+    }
+}
