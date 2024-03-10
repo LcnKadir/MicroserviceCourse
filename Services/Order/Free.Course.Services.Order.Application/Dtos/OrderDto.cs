2c49aadd68db903296b06c126783f8acbc0d378b
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-10 21:56:29 +0300

    ApplicationLibrary (DTOs)
    Dto's of related Entities were created in Free.Course.Services.Order.Application. // Free.Course.Services.Order.Application’ da ilgili Entity’lerin Dto’ları oluşturuldu.

---
+++ Services/Order/Free.Course.Services.Order.Application/Dtos/OrderDto.cs
@@ -0,0 +1,19 @@
+﻿using Free.Course.Services.Order.Domain.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Free.Course.Services.Order.Application.Dtos
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public AddressDto Address { get; set; }
+        public string BuyerId { get; set; }
+
+        public List<OrderItem> OrderItems;
+    }
+}
