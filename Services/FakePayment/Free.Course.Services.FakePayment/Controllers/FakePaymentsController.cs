d1c45a5ed34816af6b0366b6b57e1ac76f53eb58
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-11 12:49:50 +0300

    FakePaymetnsController Created

---
+++ Services/FakePayment/Free.Course.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,20 @@
+﻿using FreeCourse.Shared.BaseController;
+using FreeCourse.Shared.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Free.Course.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance(Response<NoContent>.Success(200));
+
+        }
+    }
+}
