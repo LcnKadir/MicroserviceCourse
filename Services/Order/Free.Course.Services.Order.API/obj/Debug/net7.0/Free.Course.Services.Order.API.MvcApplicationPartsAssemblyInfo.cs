aa299ff3c930396c40818f94fa905ab7afcb04a9
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-11 10:12:02 +0300

    MediatR (Query/QueryHandler)

--- Services/Order/Free.Course.Services.Order.API/obj/Debug/net7.0/Free.Course.Services.Order.API.MvcApplicationPartsAssemblyInfo.cs
+++ Services/Order/Free.Course.Services.Order.API/obj/Debug/net7.0/Free.Course.Services.Order.API.MvcApplicationPartsAssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Free.Course.Services.Order.Application")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("FreeCourse.Shared")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.OpenApi")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
---
+++ Services/Order/Free.Course.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using Free.Course.Services.Order.Application.Dtos;
+using FreeCourse.Shared.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Free.Course.Services.Order.Application.Queries
+{
+     class GetOrdersByUserIdQuery:IRequest<Response<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
