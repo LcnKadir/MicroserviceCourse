46a949aac0d286168142cf12752aaf9052b74713
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-11 13:31:06 +0300

    Free.Course.Services.Order.FakePayment Microservice has been secured and tested with postman. // Free.Course.Services.Order.FakePayment Microservice’i koruma altına alındı ve postman ile test işlemleri gerçekleşti.

--- Services/FakePayment/Free.Course.Services.FakePayment/Program.cs
+++ Services/FakePayment/Free.Course.Services.FakePayment/Program.cs
@@ -1,11 +1,33 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+//JSONWEBTOKEN
+//Microservice is protected. 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_payment";
+    options.RequireHttpsMetadata = false;
+});
+
 
 var app = builder.Build();
 
@@ -15,7 +37,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
