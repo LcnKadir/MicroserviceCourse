ece83a0c6b3e2c3f9636a24f53709b8e0ca757c9
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-11 14:26:03 +0300

    Configuration.development.json/Configuration.production.json
    According to the requests, json files that will be routed to microservices were written and added to DI Container. // Request’lere göre microservic’lere yönlendirme yapılacak olan json dosyları yazıldı ve DI Container’a eklemeleri yapıldı.

--- Free.Course.Gateway/Program.cs
+++ Free.Course.Gateway/Program.cs
@@ -3,10 +3,11 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddJsonFile($"configuration.{builder.Environment.EnvironmentName.ToLower()}.json");
+
+
+
 builder.Services.AddOcelot();
-
-
-
 
 
 var app = builder.Build();
