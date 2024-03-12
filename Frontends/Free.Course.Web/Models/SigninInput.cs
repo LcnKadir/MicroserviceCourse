95c470186696c4dc3fb7e4684fb2938442328ace
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-12 15:21:38 +0300

    IdentityService/IdentityModel Library

---
+++ Frontends/Free.Course.Web/Models/SigninInput.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Free.Course.Web.Models
+{
+    public class SignInİnput
+    {
+        //Data will be taken from the user. //Kullanıcıdan data alınacak.
+
+        [Display(Name ="Email adresiniz")]
+        public string Email { get; set; }
+
+        [Display(Name ="Şifreniz")]
+        public string Password { get; set; }
+
+        [Display(Name ="Beni Hatırla")]
+        public bool IsRemember { get; set; }
+    }
+}
---
+++ Frontends/Free.Course.Web/Services/Interfaces/IdentityService.cs
@@ -0,0 +1,14 @@
+﻿using FreeCourse.Shared.DTOs;
+using IdentityModel.Client;
+
+namespace Free.Course.Web.Services.Interfaces
+{
+    interface IdentityService
+    {
+        Task<Response<bool>> SignIn();
+
+        Task<TokenResponse> GetAccesTokenByRefreshToken();
+
+        Task RevokeRefreshToken();
+    }
+}
