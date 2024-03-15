c4b95289e255b3bcf545f8e208f440ed1c4f0ebc
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-15 13:59:01 +0300

    IPhotoStockService and PhotoController(Microservice) arrangements

--- Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -38,7 +38,7 @@ namespace FreeCourse.Services.PhotoStock.Controllers
         [HttpDelete]
         public IActionResult PhotoDelete(string photoUrl)
         {
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", photoUrl);
 
             if (!System.IO.File.Exists(path))
             {
