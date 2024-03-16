dfd76f1f6bdfface1160995973d846ce3726e594
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-16 16:19:57 +0300

    BasketController

---
+++ Frontends/Free.Course.Web/Controllers/BasketController.cs
@@ -0,0 +1,41 @@
+﻿using Free.Course.Web.Models.Baskets;
+using Free.Course.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Free.Course.Web.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+
+        public BasketController(ICatalogService catalogService, IBasketService basketService)
+        {
+            _catalogService = catalogService;
+            _basketService = basketService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _basketService.Get());
+        }
+
+        public async Task<IActionResult> AddBasketItem(string courseId)
+        {
+            var course = await _catalogService.GetByCourseId(courseId);
+
+            var basketItem = new BasketItemViewModel { CourseId = course.Id, CourseName = course.Name, Price = course.Price };
+
+            await _basketService.AddBasketItem(basketItem);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> RemoveBasketItem(string courseId)
+        {
+            await _basketService.RemoveBasketItem(courseId);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Frontends/Free.Course.Web/Models/Baskets/BasketItemViewModel.cs
+++ Frontends/Free.Course.Web/Models/Baskets/BasketItemViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class BasketItemViewModel
     {
-        public int Quantity { get; set; }
+        public int Quantity { get; set; } = 1;
         public string CourseId { get; set; }
         public string CourseName { get; set; }
         public decimal Price { get; set; }
