c3d7479ad85ab75fe2d5b5cc88baa9aa115e797d
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-19 18:35:35 +0300

    Connection Order Microservice -- [ChechoutInfoInput]

---
+++ Frontends/Free.Course.Web/Models/Ordes/ChechoutInfoInput.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Free.Course.Web.Models.Ordes
+{
+    public class ChechoutInfoInput
+    {
+        [Display(Name = "İl")]
+        public string Province { get; set; }
+
+        [Display(Name = "İlçe")]
+        public string District { get; set; }
+
+        [Display(Name = "Cadde")]
+        public string Street { get; set; }
+
+        [Display(Name = "Posta Kodu")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Adres")]
+        public string Line { get; set; }
+
+        [Display(Name = "Kart İsim Soy İsim")]
+        public string CardName { get; set; }
+
+        [Display(Name = "Kart Numarası")]
+        public string CardNumber { get; set; }
+
+        [Display(Name = "Son kullanma tarihi")]
+        public string Expiration { get; set; }
+
+        [Display(Name = "CVV/CVC2 numarası")]
+        public string CVV { get; set; }
+
+    }
+}
