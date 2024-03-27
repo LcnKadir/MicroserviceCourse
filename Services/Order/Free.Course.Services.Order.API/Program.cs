66140309af6a69f7bed711df30f1960bb316d65e
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-27 16:16:29 +0300

    Client [Asp.Net Core Mvc] →   DockerFile / docker-compose test and edits

--- Services/Order/Free.Course.Services.Order.API/Program.cs
+++ Services/Order/Free.Course.Services.Order.API/Program.cs
@@ -84,6 +84,14 @@ builder.Services.AddMassTransit(x =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    var orderDbContext = serviceProvider.GetRequiredService<OrderDbContext>();
+    orderDbContext.Database.Migrate();
+
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
